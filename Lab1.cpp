5cc40d87bd6ee5ba62ac9859c045ba8a853d3e21
RybOlya <56222578+RybOlya@users.noreply.github.com>
2022-03-29 16:39:42 +0300

    Appliable with vectors

--- Lab1.cpp
+++ Lab1.cpp
@@ -2,60 +2,67 @@
 #include <vector>
 
 using namespace std;
-void dopovn(int U[], int compset[], int n, int m)
+void dopovn(vector<int> U, vector<int> A)
 {
     vector<int> res;
     int i = 0, j = 0;
-    while (i < n && j < m) {
-        if (compset[i] < U[j]) {
-            res.push_back(compset[i]);
+    while (i < A.size() && j < U.size()) {
+        if (A[i] < U[j]) {
+            res.push_back(A[i]);
             i++;
         }
-        else if (compset[i] > U[j]) {
+        else if (A[i] > U[j]) {
             j++;
         }
-        else if (compset[i] == U[j]) {
+        else if (A[i] == U[j]) {
             i++;
             j++;
         }
     }
-    while (i < n){
-        cout << compset[i] << " ";
-        res.push_back(compset[i]);
+    while (i < A.size()){
+        res.push_back(A[i]);
         i++;
     }
-
+    cout << "\nSet difference of U and A:\n  ";
     for (int& c : res) {
         cout <<c << ",";
     }
 }
-void cartesian(int A[], int B[], int n, int m)
+void decart(vector<int> A, vector<int> B)
 {
-    vector<pair<int, int>> decart;
-    for (int i = 0; i < n; i++)
+    vector<pair<int, int>> cart;
+    for (int i = 0; i < A.size(); i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < B.size(); j++)
         {
-            decart.push_back(make_pair(A[i], B[j]));
+            cart.push_back(make_pair(A[i], B[j]));
         }
     }
-    cout << "Cartesian product of {";
-    for (auto &el: decart)
+    cout << "\nCartesian product:\n";
+    for (auto &el: cart)
     {
         cout <<"{" << el.first << "," << el.second <<"}";
     }
+    cout << "\n";
 }
 int main() {
-    int set1[] = { 1,2,4 ,5, 6, 8, 10, 11, 13 };
-    int set2[] = { 1, 2, 3, 4, 5, 6};
-    int U[] = { 1, 3, 4, 6, 9, 12};
+    vector<int> set1 = { 2, 5, 10, 12 };
+    vector<int> set2 = { 1, 2, 5, 6};
+    vector<int> U = { 1, 3, 4, 6, 9, 12};
+    cout << "Set A:\n  ";
+    for (int& s : set1) {
+        cout << s << ",";
+    }
+    cout << "\nSet B:\n  ";
+    for (int& s : set2) {
+        cout << s << ",";
+    }
+    cout << "\nSet U:\n  ";
+    for (int& s : U) {
+        cout << s << ",";
+    }
     
-    int size1 = sizeof(set1) / sizeof(set1[0]);
-    int size2 = sizeof(set2) / sizeof(set2[0]);
-    int sizeU = sizeof(U) / sizeof(U[0]);
+    dopovn(U,set1);
+    decart(set1, set2);
     
-    dopovn(U,set1,  size1,sizeU);
-    cartesian(set1, set2, size1, size2);
-    
-
 }
